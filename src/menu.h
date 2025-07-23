17d3518ad34694228e9929f52f4cfb810af2e816
Dero35 <antek.ciesielski.2006@gmail.com>
2025-07-23 21:36:59 +0200

    minor fixes

--- src/menu.h
+++ src/menu.h
@@ -5,6 +5,7 @@
 
 // Menu states
 enum AppState {
+  STATE_INTRO,
   STATE_MENU,
   STATE_PAGE_INFO,
   STATE_PAGE_BASIC_DATA,
@@ -16,6 +17,11 @@ enum AppState {
 extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
 extern AppState current_state;
 extern int item_selected;
+extern bool just_entered_page;
+extern unsigned long page_enter_time;
+extern unsigned long last_button_time;
+extern bool button_select_pressed;
+extern const unsigned long DEBOUNCE_DELAY;
 
 // Function declarations
 void drawMenu();
