ddfde78d656958a0f2e7c2814b3bcc8937016bc7
Dero35 <antek.ciesielski.2006@gmail.com>
2025-07-23 22:26:47 +0200

    basic data screen

--- src/pages/page_basic_data.h
+++ src/pages/page_basic_data.h
@@ -3,6 +3,10 @@
 
 #include "../menu.h"
 
+// Bitmap definitions
+static const unsigned char image_OIL_bits[] = {0x04,0x0a,0x0a,0x11,0x11,0x0e};
+static const unsigned char image_SNOWFLAKE_bits[] = {0x15,0x0e,0x1b,0x0e,0x15};
+
 extern bool button_select_pressed;
 extern unsigned long last_button_time;
 extern bool just_entered_page;
@@ -11,19 +15,56 @@ extern const unsigned long DEBOUNCE_DELAY;
 
 void drawPageBasicData() {
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_6x10_tr);
+  u8g2.setFontMode(1);
+  u8g2.setBitmapMode(1);
   
-  // Title
-  u8g2.drawStr(10, 15, "Basic Data Page");
-  
-  // Content
+  // RPM NUM
+  u8g2.setFont(u8g2_font_profont15_tr);
+  u8g2.drawStr(10, 24, "0000");
+
+  // COOLANT NUM
+  u8g2.drawStr(10, 40, "000");
+
+  // Layer 4 copy
   u8g2.setFont(u8g2_font_4x6_tr);
-  u8g2.drawStr(10, 30, "Basic vehicle data");
-  u8g2.drawStr(10, 40, "and measurements.");
-  
-  // Back instruction
-  u8g2.drawStr(10, 55, "Press SELECT to go back");
-  
+  u8g2.drawStr(32, 40, "C");
+
+  // RPM TEXT
+  u8g2.drawStr(39, 24, "RPM");
+
+  // OIL NUM
+  u8g2.setFont(u8g2_font_profont15_tr);
+  u8g2.drawStr(10, 55, "000");
+
+  // SNOWFLAKE
+  u8g2.drawXBM(38, 35, 5, 5, image_SNOWFLAKE_bits);
+
+  // Layer 4 copy
+  u8g2.setFont(u8g2_font_4x6_tr);
+  u8g2.drawStr(32, 55, "C");
+
+  // SPEED
+  u8g2.setFont(u8g2_font_profont15_tr);
+  u8g2.drawStr(67, 24, "000");
+
+  // OIL
+  u8g2.drawXBM(38, 49, 5, 6, image_OIL_bits);
+
+  // KMPH
+  u8g2.setFont(u8g2_font_4x6_tr);
+  u8g2.drawStr(89, 24, "KM/H");
+
+  // PLACE HOLDER2
+  u8g2.setFont(u8g2_font_profont15_tr);
+  u8g2.drawStr(67, 40, "000");
+
+  // PLACE HOLDER1
+  u8g2.drawStr(67, 55, "000");
+
+  // Layer 13
+  u8g2.setFont(u8g2_font_4x6_tr);
+  u8g2.drawStr(10, 9, "Basic Data");
+
   u8g2.sendBuffer();
 }
 
