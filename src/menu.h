8799d730795c5193c402f6ba966de7c3b3d75382
Dero35 <antek.ciesielski.2006@gmail.com>
2025-07-23 19:24:04 +0200

    Working menu with changing pages functionality

---
+++ src/menu.h
@@ -0,0 +1,24 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <U8g2lib.h>
+
+// Menu states
+enum AppState {
+  STATE_MENU,
+  STATE_PAGE_INFO,
+  STATE_PAGE_BASIC_DATA,
+  STATE_PAGE_OIL_TEMP,
+  STATE_PAGE_RPM
+};
+
+// External variables
+extern U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2;
+extern AppState current_state;
+extern int item_selected;
+
+// Function declarations
+void drawMenu();
+void handleMenuButtons();
+
+#endif
---
+++ src/pages/page_oil_temp.h
@@ -0,0 +1,52 @@
+#ifndef PAGE_OIL_TEMP_H
+#define PAGE_OIL_TEMP_H
+
+#include "../menu.h"
+
+extern bool button_select_pressed;
+extern unsigned long last_button_time;
+extern bool just_entered_page;
+extern unsigned long page_enter_time;
+extern const unsigned long DEBOUNCE_DELAY;
+
+void drawPageOilTemp() {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x10_tr);
+  
+  // Title
+  u8g2.drawStr(10, 15, "Oil Temp Page");
+  
+  // Content
+  u8g2.setFont(u8g2_font_4x6_tr);
+  u8g2.drawStr(10, 30, "Oil temperature");
+  u8g2.drawStr(10, 40, "monitoring display.");
+  
+  // Back instruction
+  u8g2.drawStr(10, 55, "Press SELECT to go back");
+  
+  u8g2.sendBuffer();
+}
+
+void handlePageOilTemp() {
+  unsigned long current_time = millis();
+  
+  // Clear the just_entered_page flag after a delay
+  if (just_entered_page && (current_time - page_enter_time > DEBOUNCE_DELAY)) {
+    just_entered_page = false;
+  }
+  
+  // Only handle button if we haven't just entered the page and enough time has passed
+  if (!just_entered_page && (current_time - last_button_time > DEBOUNCE_DELAY)) {
+    if (digitalRead(5) == LOW && !button_select_pressed) { // BUTTON_SELECT
+      button_select_pressed = true;
+      last_button_time = current_time;
+      current_state = STATE_MENU;
+      Serial.println("Returning to menu from Oil Temp page");
+    }
+    else if (digitalRead(5) == HIGH) {
+      button_select_pressed = false;
+    }
+  }
+}
+
+#endif
