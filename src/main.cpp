9fc34227999ba12135c56c2c516324d94d79f614
Dero35 <antek.ciesielski.2006@gmail.com>
2026-03-24 18:11:58 +0100

    LVGL graphics

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #define LGFX_USE_V1
 #include <LovyanGFX.hpp>
-#include <bitmaps.h>
+// #include <bitmaps.h> // Zakładamy, że obrazki przekonwertowałeś na .c w folderze src
+#include <lvgl.h>
 
 class LGFX : public lgfx::LGFX_Device {
     lgfx::Panel_ST7789 _panel_instance;
@@ -46,89 +47,138 @@ public:
 
 LGFX tft;
 
-#define BTN_1 4
-#define BTN_2 5
-#define PRIMARY_COLOR 0xEAE0
+#define BTN_1 5
+#define BTN_2 4
 
-int cursor = 1;
-int old_cursor = 1;
+LV_IMG_DECLARE(voltage_active);
+LV_IMG_DECLARE(voltage_n_active);
 
-#define MENU_ITEMS_NUM 3
-#define MENU_WIDTH 288
-#define MENU_HEIGHT 32
+LV_IMG_DECLARE(rpm_active);
+LV_IMG_DECLARE(rpm_n_active);
 
-void draw_loading_screen() {
-    tft.fillScreen(0x0000);         
-    tft.drawBitmap(68, 28, image_bmw_logo_own_bits, 183, 183, 0xEAE0);
-}
+LV_IMG_DECLARE(cool_active);
+LV_IMG_DECLARE(cool_n_active);
 
-void draw_button(int x, int y, const unsigned char* bitmap) {    
-    tft.drawBitmap(x, y, bitmap, MENU_WIDTH, MENU_HEIGHT, PRIMARY_COLOR, 0x0000);
-}
+static const uint32_t screenWidth = 320;
+static const uint32_t screenHeight = 240;
 
-void draw_menu(bool force_all = false) {
+// --- OPTYMALIZACJA: Podwójne buforowanie ---
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf_1[screenWidth * screenHeight / 10];
+static lv_color_t buf_2[screenWidth * screenHeight / 10];
+
+void disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p) {
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+
     tft.startWrite();
+    tft.setAddrWindow(area->x1, area->y1, w, h);
+    // OPTYMALIZACJA: Wypychamy paczkę przez DMA
+    tft.pushPixelsDMA((lgfx::rgb565_t *)&color_p->full, w * h);
+    tft.endWrite();
 
-    if (force_all) {
-        if (cursor == 1) draw_button(15, 20, image_voltage_active_bits);
-        else             draw_button(15, 20, image_voltage_n_active_bits);
+    lv_disp_flush_ready(disp_drv);
+}
 
-        if (cursor == 2) draw_button(15, 64, image_rpm_active_bits);
-        else             draw_button(15, 64, image_rpm_n_active_bits);
+void button_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data) {
+    static uint32_t last_key = 0;
 
-        if (cursor == 3) draw_button(15, 108, image_cool_active_bits);
-        else             draw_button(15, 108, image_cool_n_active_bits);
+    if (digitalRead(BTN_1) == LOW) {
+        data->state = LV_INDEV_STATE_PR;
+        last_key = LV_KEY_PREV;
+    }
+    else if (digitalRead(BTN_2) == LOW) {
+        data->state = LV_INDEV_STATE_PR;
+        last_key = LV_KEY_NEXT;
     }
     else {
-        if (old_cursor == 1) draw_button(15, 20, image_voltage_n_active_bits);
-        if (old_cursor == 2) draw_button(15, 64, image_rpm_n_active_bits);
-        if (old_cursor == 3) draw_button(15, 108, image_cool_n_active_bits);
+        data->state = LV_INDEV_STATE_REL;
+    }
 
-        if (cursor == 1) draw_button(15, 20, image_voltage_active_bits);
-        if (cursor == 2) draw_button(15, 64, image_rpm_active_bits);
-        if (cursor == 3) draw_button(15, 108, image_cool_active_bits);
-    } 
+    data->key = last_key;
+}
 
-    tft.endWrite();
+uint32_t custom_tick_get(void) {
+    return millis();
 }
 
 void setup() {
     Serial.begin(115200);
-    delay(1000); 
 
     pinMode(BTN_1, INPUT_PULLUP);
     pinMode(BTN_2, INPUT_PULLUP);
 
     tft.init();
     tft.setRotation(3);
-
-    draw_loading_screen();
-    delay(3000);
-
     tft.fillScreen(0x0000);
-    draw_menu(true);
+
+    lv_init();
+    
+    // Inicjalizacja podwójnego bufora
+    lv_disp_draw_buf_init(&draw_buf, buf_1, buf_2, screenWidth * screenHeight / 10);
+
+    // Register screen
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    // Register btns
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_KEYPAD;
+    indev_drv.read_cb = button_read;
+    lv_indev_t * my_indev = lv_indev_drv_register(&indev_drv);
+
+    // Group
+    lv_group_t * g = lv_group_create();
+    lv_group_set_default(g);
+    lv_indev_set_group(my_indev, g);
+
+    static lv_style_t style_img_btn;
+    lv_style_init(&style_img_btn);
+    lv_style_set_bg_opa(&style_img_btn, LV_OPA_0); 
+    lv_style_set_border_opa(&style_img_btn, LV_OPA_0); 
+    lv_style_set_shadow_opa(&style_img_btn, LV_OPA_0);
+    
+    // Tło ekranu na czarno
+    lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x000000), 0);
+
+    // Voltage btn    
+    lv_obj_t * btn_voltage = lv_btn_create(lv_scr_act());
+    lv_obj_remove_style_all(btn_voltage); 
+    lv_obj_set_size(btn_voltage, 288, 32); 
+    lv_obj_align(btn_voltage, LV_ALIGN_TOP_MID, 0, 20);
+    
+    lv_obj_set_style_bg_img_src(btn_voltage, &voltage_n_active, LV_STATE_DEFAULT); 
+    lv_obj_set_style_bg_img_src(btn_voltage, &voltage_active, LV_STATE_FOCUSED); 
+    lv_obj_set_style_bg_img_src(btn_voltage, &voltage_active, LV_STATE_PRESSED); 
+
+    // RPM btn
+    lv_obj_t * btn_rpm = lv_btn_create(lv_scr_act());
+    lv_obj_remove_style_all(btn_rpm);
+    lv_obj_set_size(btn_rpm, 288, 32);
+    lv_obj_align(btn_rpm, LV_ALIGN_TOP_MID, 0, 64);
+
+    lv_obj_set_style_bg_img_src(btn_rpm, &rpm_n_active, LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_img_src(btn_rpm, &rpm_active, LV_STATE_FOCUSED);
+    lv_obj_set_style_bg_img_src(btn_rpm, &rpm_active, LV_STATE_PRESSED);
+
+    // Coolant btn
+    lv_obj_t * btn_cool = lv_btn_create(lv_scr_act());
+    lv_obj_remove_style_all(btn_cool);
+    lv_obj_set_size(btn_cool, 288, 32);
+    lv_obj_align(btn_cool, LV_ALIGN_TOP_MID, 0, 108);
+
+    lv_obj_set_style_bg_img_src(btn_cool, &cool_n_active, LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_img_src(btn_cool, &cool_active, LV_STATE_FOCUSED);
+    lv_obj_set_style_bg_img_src(btn_cool, &cool_active, LV_STATE_PRESSED);
 }
 
-void loop() {    
-    if (digitalRead(BTN_2) == LOW) {
-        delay(30);
-        if (digitalRead(BTN_2) == LOW) {
-            old_cursor = cursor;
-            cursor--;
-            if (cursor < 1) cursor = MENU_ITEMS_NUM;
-            draw_menu(false);
-        }
-        while (digitalRead(BTN_2) == LOW) {delay(10);}
-    }
-
-    if (digitalRead(BTN_1) == LOW) {
-        delay(30);
-        if (digitalRead(BTN_1) == LOW) {
-            old_cursor = cursor;
-            cursor++;
-            if (cursor > MENU_ITEMS_NUM) cursor = 1;
-            draw_menu(false);
-        }
-        while (digitalRead(BTN_1) == LOW) {delay(10);}
-    }
+void loop() {
+    lv_timer_handler();
+    delay(5);
 }
